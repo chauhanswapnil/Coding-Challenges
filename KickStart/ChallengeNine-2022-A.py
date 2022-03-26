e960cd5b5de2f728dc8aa7aece2b26c2300a9279
Swapnil Chauhan <swapnilchauhan999@gmail.com>
2022-03-26 11:26:33 +0000

    First Commit

---
+++ KickStart/ChallengeNine-2022-A.py
@@ -0,0 +1,6 @@
+'''
+https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb33e/00000000009e7997
+
+
+
+'''
---
+++ KickStart/SpeedTyping-2022-A.py
@@ -0,0 +1,33 @@
+'''
+0 -> I, I newStr-> I
+1 -> C=1
+https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb33e/00000000009e7021
+'''
+def speedtyping(I,P):
+    if len(P) < len(I):
+        return -1
+    C=0
+    i = 0
+    j = 0
+    while (i < len(I) and j < len(P)):
+        if I[i] == P[j]:
+            i+=1
+            j+=1
+        else:
+            j+=1
+            C+=1
+    if i == len(I):
+        return (len(P) - len(I))
+    else:
+        return -1
+    
+num_cases = int(input())
+
+for case in range(1, num_cases + 1):
+    I = input()
+    P = input()
+    C = speedtyping(I, P)
+    result = "IMPOSSIBLE" if C == -1 else C
+    print('Case #{}: {}'.format(case, result))
+    
+# print(speedtyping("hello", "hiello"))
